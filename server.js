db020fdb9659768216e1bf628a24877f6ac09d51
Blake Georgii <blakegeorgii@gmail.com>
2022-01-28 18:33:57 -0800

    created routes files

--- server.js
+++ server.js
@@ -1,24 +1,12 @@
 const express = require('express');
-const mysql = require('mysql2');
 const inputCheck = require('./utils/inputCheck');
+const db = require('./db/connection');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-
-const db = mysql.createConnection(
-    {
-        host: 'localhost',
-        // Your MySQL username,
-        user: 'root',
-        // Your MySQL password
-        password: '',
-        database: 'election'
-    },
-    console.log('Connected to the election database.')
-);
 
 app.get('/api/candidates', (req, res) => {
     const sql = `SELECT candidates.*, parties.name 
